f1621870ee8b5293c76cc45c507834930758f31c
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2017-08-01 13:35:14 -0400

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+let shelljs = require('shelljs');
+
+////// A) Source File //////
+
+// A1 - user specified which file should be used
+
+  // if (!fileName) error(file not found)
+
+  // unpack all functions in the file
