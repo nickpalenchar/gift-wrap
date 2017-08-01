7d5bf024dc2fcb4395c68b55c132a90e5fe0b340
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2017-08-01 13:47:53 -0400

    Update functionUnpacker.js

--- bin/functionUnpacker.js
+++ bin/functionUnpacker.js
@@ -1,1 +1,20 @@
-
+export function functionUnpacker(filePath){
+  
+  import * as fn from filePath;
+  let entryFn;
+  
+  if(Object.keys(fn) > 1){
+    console.log("More than one function exported");
+    //PROMPT for correct function. (FILTER non-functions)
+  }
+  else {
+    // there is only one function located at the 0th index.
+    entryFn = fn[Object.keys(fn)[0]];
+  }
+  
+  if(typeof entryFn !== 'function'){
+    throw new TypeError("needs to be function"); //TODO ELABORATE
+  }
+  
+  return fn;
+}
