882d9e618561f4ede9486f4370f0e56b8ee6fb26
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2017-08-01 13:49:37 -0400

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,7 @@
 let shelljs = require('shelljs');
 
+import { functionUnpacker } from './functionUnpacker';
+
 ////// A) Source File //////
 
 // A1 - user specified which file should be used
@@ -7,3 +9,7 @@ let shelljs = require('shelljs');
   // if (!fileName) error(file not found)
 
   // unpack all functions in the file
+
+let entryFn = functionUnpacker('TODO: get path from args or interact');
+
+
