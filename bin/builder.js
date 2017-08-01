e687f8c144456a027490cfd0d1a6a9ee7e90390f
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2017-08-01 14:53:45 -0400

    Create builder.js

---
+++ bin/builder.js
@@ -0,0 +1,24 @@
+let shelljs = require('shelljs);
+let path = require('path')
+
+
+export function builder(entryFn, cliName){
+  let outpath = ''//TODO GET OUTPATH
+
+  let outfile = '';
+
+  // add arg parcing. refernced as array `args`
+  outfile += shelljs.cat(path.join(__dirname, 'fragments/prepareArgs.js')).stdout;
+
+  //if the function is anonymous, need to assign it to a variable, which will be the reference to invoke
+  let entryFnName = entryFn.name || "genericFn";
+  
+  outfile +=
+```
+var ${entryFnName} = ${entryFn.toString()};
+${entryFnName}(...args);
+```
+  
+  // write file to the output. Just be a man and use fs module.
+  
+}
